3f9ba392858635477373804e771f662641796a59
Leandro Carvalho <contato@leandrowebdev.net>
2019-10-03 12:13:17 -0300

    Update Filtros.js

--- resources/js/src/components/Filtros.js
+++ resources/js/src/components/Filtros.js
@@ -15,9 +15,8 @@ const Filtro = ({ perPage, handlePerPage, filtro, handleFiltro }) => {
                     id="perpage"
                 >
                     <option value="5">5</option>
+                    <option value="10">10</option>
                     <option value="20">20</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
                 </select>
             </div>
 
